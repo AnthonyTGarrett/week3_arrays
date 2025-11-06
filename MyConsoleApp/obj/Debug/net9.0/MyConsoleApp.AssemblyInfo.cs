5a94de7272338d3d0d367413c9ba71afcec54d36
Anthony Garrett <tony.garrett@gmail.com>
2025-11-06 17:38:50 -0500

    updating gitignore

--- MyConsoleApp/obj/Debug/net9.0/MyConsoleApp.AssemblyInfo.cs
+++ MyConsoleApp/obj/Debug/net9.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb08c71e6b04b508cefda6656e49c2103b570269")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f99e277e0be66d9cd4d8ea39a6a5eb61f1dde2c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
