83d703b912c3ff093093d7c136a7d2a11487abce
Anthony Garrett <tony.garrett@gmail.com>
2025-11-06 16:50:55 -0500

    Basic program working

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Provide a sentence and I will capitalize all of the words:");
+string input = Console.ReadLine();
+string cleanedInput = input.Trim();
+
+string[] words = cleanedInput.Split(' ');
+Console.Clear();
+
+foreach (string word in words)
+{
+    Console.Write(char.ToUpper(word[0]) + word.Substring(1) + ' ' );
+}
+
